72a55961260f2654fbc940cb70dce17b16c035a4
ReoKano <reo-kano@sws.com>
2025-05-27 12:56:34 +0900

    自動テスト前に削除

--- 00_hiddenUtf8/hidden_utf-8.c
+++
@@ -1,60 +0,0 @@
-＜Unicode テストサンプル＞
-Code,Symbol,Description,Translation,Sample Output
-U+0000,NUL,NULL,ヌル文字,"�" (U+0000)
-U+0001,SOH,Start of Heading,メッセージの先頭,"" (U+0001)
-U+0002,STX,Start of Text,テキスト開始, "" (U+0002)
-U+0003,ETX,End of Text,テキスト終了,"" (U+0003)
-U+0004,EOT,End of Transmission,伝送終了,"" (U+0004)
-U+0005,ENQ,Enquiry,問合せ,"" (U+0005)
-U+0006,ACK,Acknowledge,確認応答,"" (U+0006)
-U+0007,BEL,Bell,警告音,"" (U+0007)
-U+0008,BS,Backspace,バックスペース,"" (U+0008)
-U+000A,LF,Line Feed,ラインフィード," " (U+000A)
-U+000B,VT,Vertical Tab,垂直タブ, "" (U+000B)
-U+000C,FF,Form Feed,ページ送り,"" (U+000C)
-U+000D,CR,Carriage Return,キャリッジリターン," " (U+000D)
-U+000E,SO,Shift Out,表示の切り替え, "" (U+000E)
-U+000F,SI,Shift In,表示の切り替え,"" (U+000F)
-U+0010,DLE,Data Link Escape,データリンクエスケープ,"" (U+0010)
-U+0011,DC1,Device Control 1,デバイス制御1,"" (U+0011)
-U+0012,DC2,Device Control 2,デバイス制御2,"" (U+0012)
-U+0013,DC3,Device Control 3,デバイス制御3,"" (U+0013)
-U+0014,DC4,Device Control 4,デバイス制御4,"" (U+0014)
-U+0015,NAK,Negative Acknowledge,否定確認,"" (U+0015)
-U+0016,SYN,Synchronous Idle,同期待機,"" (U+0016)
-U+0017,ETB,End of Transmission Block,終端ブロック, "" (U+0017)
-U+0018,CAN,Cancel,キャンセル,"" (U+0018)
-U+0019,EM,End of Medium,メディア終了,"" (U+0019)
-U+001A,SUB,Substitute,代替,"" (U+001A)
-U+001B,ESC,Escape,エスケープ,"" (U+001B)
-U+001C,FS,File Separator,ファイル区切り,"" (U+001C)
-U+001D,GS,Group Separator,グループ区切り,"" (U+001D)
-U+001E,RS,Record Separator (Block separator),レコード区切り, 記録1記録2
-U+001F,US,Unit Separator (Segment separator),ユニット区切り, セグメントAセグメントB
-U+00A0,NBS,No-Break Space,改行を避ける空白,"" (U+00A0)  
-U+200B,ZWSP,Zero width Space,ゼロ幅スペース, "​" (U+200B)
-U+200C,ZWNJ,Zero width non-joiner,ゼロ幅非結合子, ن‌ـ
-U+200D,ZWJ,Zero width joiner,ゼロ幅結合子, ن‍ـ
-U+200E,LRM,Left-to-right mark,左から右のマーク, ‎これはLTRテキストです。
-U+200F,RLM,Right-to-left mark,右から左のマーク, ‏זה טקסט בעברית.
-U+202A,LRE,Start of left-to-right embedding,左から右への埋め込み, ‪مرحبا بالعالم!‬
-U+202B,RLE,Start of right-to-left embedding,右から左への埋め込み, ‫Hello, world!‬
-U+202C,PDF,Pop directional formatting,方向フォーマット終了, ‪Left to Right‬ 終了後通常表示。
-U+202D,LRO,Start of left-to-right override,左から右の強制, ‭مرحبا بالعالم!‬
-U+202E,RLO,Start of right-to-left override,右から左の強制, ‮Hello, world!‬
-U+2060,WJ,Word Joiner,改行を許可しないスペース,"⁠" (U+2060)
-U+2061,FA,FUNCTION APPLICATION,関数の適用を示す連続演算子,"⁡" (U+2061)
-U+2062,IT,INVISIBLE TIMES,乗算を表す連続演算子,"⁢" (U+2062)
-U+2063,IS,INVISIBLE SEPARATOR,リストを形成する数学記号,"⁣" (U+2063)
-U+2064,IP,INVISIBLE PLUS,加算を示す連続演算子,"⁤" (U+2064)
-U+2066,LRI,LEFT-TO-RIGHT ISOLATE,左横書き独立,"⁦" (U+2066)
-U+2067,RLI,RIGHT-TO-LEFT ISOLATE,右横書き独立,"⁧" (U+2067)
-U+2068,FSI,FIRST STRONG ISOLATE,最初の書字方向が定義された文字の書字方向に統一,"⁨" (U+2068)
-U+2069,PDI,POP DIRECTIONAL ISOLATE,書字方向の隔離を解除,"⁩" (U+2069)
-U+206A,ISS,Inhibit symmetric swapping,対称記号の置換無効, ⁫<‬>
-U+206B,ASS,Activate symmetric swapping,対称記号の置換有効, ⁪<‬>
-U+206C,IAFS,Inhibit Arabic from shaping,アラビア文字の形状変化抑制, ⁭سلام
-U+206D,AAFS,Activate Arabic form shaping,アラビア文字の形状変化有効, ⁬سلام⁭
-U+206E,NADS,National digit shapes substitution,国別数字形状の置換, ⁮123456⁯
-U+206F,NODS,Nominal (European) digit shapes,標準（欧州）数字形状の置換, ⁯١٢٣٤٥٦
-
